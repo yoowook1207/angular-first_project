a8b52fa8cfa40736d279685ba35d9f682900e380
Sung Yoo <yoowook1207@gmail.com>
2022-10-10 16:01:57 -0400

    commited

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# angular-1v7l9f
+# Angular First Practice Project - Shopping Website
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-1v7l9f)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   imports: [
@@ -32,6 +33,7 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductDetailsComponent,
     CartComponent,
     ShippingComponent,
+    TestComponent,
   ],
   bootstrap: [AppComponent],
 })
